8f0e4b048d0ca0e95522f76cd581661b90d14e26
khalid Almalki <vb.95@hotmail.com>
2018-03-09 23:10:42 +0000

    add mising pom.xml

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,13 +2,13 @@ public class main {
 
     public static void main(String arg[]){
 
-        copyFile copyFile = new copyFile(" put here dir destination");
+        copyFile copyFile = new copyFile("C:\\Users\\khalid\\Desktop\\codeSmellSample\\SourceCode\\java");
 
         readFile readFile = new readFile();
 
         // read file and get the list of path names
 
-        String filename = " this is the file input which contain a list of path that needed to be copied ";
+        String filename = "C:\\Users\\khalid\\Desktop\\javaSample.csv";
         for (String path:readFile.readFile(filename)) {
 
             if (!copyFile.copyFile(path)){
